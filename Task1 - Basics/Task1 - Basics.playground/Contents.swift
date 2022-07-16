53492a5b1c494cf7261ebfe3b016b6b6310359cb
NeverRIEght <LFKomarovME@gmail.com>
2022-07-16 14:17:44 +0300

    Added Task1

---
+++ Task1 - Basics/Task1 - Basics.playground/Contents.swift
@@ -0,0 +1,18 @@
+/*
+ Task:
+ Create a student using constants and print all of data to console
+*/
+
+let name : String = "Michael"
+let surname : String = "Komarov"
+
+let height : Int16 = 182
+let weight : Int8 = 86
+let age : Int8 = 19
+
+print("Student:\n")
+print("Name: \(name)")
+print("Surname: \(surname)\n")
+print("Age: \(age)")
+print("Height: \(height)")
+print("Weight: \(weight)")
